dc85e30fef0c44b370f7c9f4f58633e740788f93
brandon-agst <brandonfriend@Brandons-MacBook-Pro.local>
2015-06-14 22:08:18 -0700

    UIImageView+AFNetworking: Fade in images

--- UIKit+AFNetworking/UIImageView+AFNetworking.m
+++ UIKit+AFNetworking/UIImageView+AFNetworking.m
@@ -150,7 +150,15 @@
                 if (success) {
                     success(urlRequest, operation.response, responseObject);
                 } else if (responseObject) {
-                    strongSelf.image = responseObject;
+                    [UIView transitionWithView:strongSelf
+                                      duration:0.3
+                                       options:UIViewAnimationOptionTransitionCrossDissolve
+                                    animations:^{
+                                            strongSelf.image = responseObject;
+                                    } completion:^(BOOL finished) {
+                                        //
+                                    }];
+//                    strongSelf.image = responseObject;
                 }
 
                 if (operation == strongSelf.af_imageRequestOperation){
